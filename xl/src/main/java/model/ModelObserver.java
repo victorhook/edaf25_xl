dd3771e900a8dbbdb5136d9f3f8cfc406f9df154
Victor <victorkrook96@gmail.com>
2021-05-03 10:54:17 +0200

    Added javadoc comments for interfaces

--- xl/src/main/java/model/ModelObserver.java
+++ xl/src/main/java/model/ModelObserver.java
@@ -1,5 +1,13 @@
 package model;
 
+/**
+ * A simple observer interface used to observe changes on a model.
+ */
 public interface ModelObserver {
-    void modelChange(CellAddress cell, String value);
+    /**
+     *
+     * @param cell The address of the cell that has changed.
+     * @param value The new value that the cell now contains.
+     */
+    void modelHasChanged(CellAddress cell, String value);
 }
--- xl/src/main/java/model/ObservableModel.java
+++ xl/src/main/java/model/ObservableModel.java
@@ -1,6 +1,25 @@
 package model;
 
+/**
+ * An interface that a model can implement to
+ */
 public interface ObservableModel {
-    void addListenever(ModelObserver observer);
-    void deleteAllListeners();
+
+    /**
+     * Adds a new observer to the model.
+     * @param observer Observer object to notify when a change has been made.
+     */
+    void addObserver(ModelObserver observer);
+
+    /**
+     * Clears all observers.
+     */
+    void clearObservers();
+
+    /**
+     * Notifies all observers for the given address.
+     * @param address The address of the cell to notify.
+     * @param newText The value the cell now contains.
+     */
+    void notifyObservers(String address, String newText);
 }
