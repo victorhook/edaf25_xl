18915825147eb8c90b34237d163e33802d9790a5
Victor <victorkrook96@gmail.com>
2021-05-03 10:16:45 +0200

    Created cell class

--- xl/src/main/java/model/Cell.java
+++ xl/src/main/java/model/Cell.java
@@ -1,2 +1,21 @@
-package model;public class Cell {
+package model;
+
+public class Cell {
+    private CellAddress address;
+    private String rawString;
+    private String calculatedString;
+
+    public Cell(int row, int col) {
+        address = new CellAddress(row, col);
+    }
+
+    public CellAddress getAddress() {
+        return address;
+    }
+
+    public void update(String rawValue, String calculatedValue) {
+        this.rawString = rawValue;
+        this.calculatedString = calculatedValue;
+    }
+
 }
